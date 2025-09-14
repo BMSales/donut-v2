2b9a3e1cdcc67de95f7bc4fcd1708e63bf01c978
BMSales <bruno.m.sales03@gmail.com>
2025-09-14 20:56:46 -0300

    ADD: operator overloads for Vec2 and Vec3

--- vectors.hpp
+++ vectors.hpp
@@ -10,6 +10,20 @@ struct Vec2 {
 	float Dot(Vec2 b){
 		return x * b.x + y * b.y;
 	};
+	
+	Vec2(){}
+	Vec2(float _x, float _y){
+		x = _x;
+		y = _y;
+	}
+
+	friend Vec2 operator+ (Vec2 lhs, const Vec2& rhs){
+		return Vec2(lhs.x + rhs.x, lhs.y + rhs.y);
+	}
+
+	friend Vec2 operator- (Vec2 lhs, const Vec2& rhs){
+		return Vec2(lhs.x - rhs.x, lhs.y - rhs.y);
+	}
 };
 
 struct Vec3 {
@@ -20,4 +34,19 @@ struct Vec3 {
 	float Dot(Vec3 b){
 		return x * b.x + y * b.y + z * b.z;
 	};
+	
+	Vec3(){}
+	Vec3(float _x, float _y, float _z){
+		x = _x;
+		y = _y;
+		z = _z;
+	}
+
+	friend Vec3 operator+ (Vec3 lhs, const Vec3& rhs){
+		return Vec3(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z);
+	}
+
+	friend Vec3 operator- (Vec3 lhs, const Vec3& rhs){
+		return Vec3(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z);
+	}
 };
